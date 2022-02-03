f03c28cb99d4b1a3a74caf365d7afbda94c7b983
azu <azuciao@gmail.com>
2022-02-03 23:52:29 +0900

    register SW

--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -1,4 +1,14 @@
 import { h, render } from "preact";
 import { App } from "./App";
 import "./App.css";
+import { useRegisterSW } from "virtual:pwa-register/preact";
 render(<App />, document.getElementById("app")!);
+const intervalMS = 60 * 60 * 1000;
+useRegisterSW({
+    onRegistered(r) {
+        r &&
+            setInterval(() => {
+                r.update();
+            }, intervalMS);
+    },
+});
