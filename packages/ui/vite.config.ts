b009525c098f053a31d8827f762e3e560345dda6
azu <azuciao@gmail.com>
2022-02-03 23:45:44 +0900

    use pwa

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [preact()],
+    plugins: [
+        preact(),
+        VitePWA({
+            manifest: {
+                background_color: "white",
+                description: "Voice note editor",
+                display: "fullscreen",
+                lang: "ja-JP",
+                icons: [
+                    {
+                        src: "https://voicod.pages.dev/icon.png",
+                        sizes: "256x256",
+                        type: "image/png",
+                    },
+                ],
+                name: "Voicod",
+                short_name: "voicod",
+                start_url: "https://voicod.pages.dev/",
+            },
+        }),
+    ],
 });
