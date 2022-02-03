e90f455cb123212fa7507710b4a9c44805751726
azu <azuciao@gmail.com>
2022-02-03 23:54:55 +0900

    move

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -1,8 +1,18 @@
 import { h, render } from "preact";
 import { VoiceEditor } from "./VoiceEditor/VoiceEditor";
 import { VoiceUI } from "./VoiceUI/VoiceUI";
+import { useRegisterSW } from "virtual:pwa-register/preact";
 
 export const App = () => {
+    const intervalMS = 60 * 60 * 1000;
+    useRegisterSW({
+        onRegistered(r) {
+            r &&
+                setInterval(() => {
+                    r.update();
+                }, intervalMS);
+        },
+    });
     return (
         <main style={{ display: "flex", flexDirection: "column" }} className={"main"}>
             <VoiceEditor style={{ flex: 1, overflow: "auto" }} />
--- packages/ui/src/main.tsx
+++ packages/ui/src/main.tsx
@@ -1,14 +1,4 @@
 import { h, render } from "preact";
 import { App } from "./App";
 import "./App.css";
-import { useRegisterSW } from "virtual:pwa-register/preact";
 render(<App />, document.getElementById("app")!);
-const intervalMS = 60 * 60 * 1000;
-useRegisterSW({
-    onRegistered(r) {
-        r &&
-            setInterval(() => {
-                r.update();
-            }, intervalMS);
-    },
-});
